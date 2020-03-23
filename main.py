f99df566454e40236b925048cf18073923ac6940
Aligh <ali.ghaderipour@gmail.com>
2020-03-23 15:29:17 +0430

    fixed bugs

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
 import random
 
-cpu_deck = ['r', 's', 'p']
+cpu_deck = ['rock', 'scissor', 'paper']
 player_score = 0
 cpu_score = 0
 entry = ['r', 'rock', 's', 'scissor', 'p', 'paper']
-rounds = input(print("how many wins to finish the game? "))
-while 1:
+flag = True
+while flag:
+    round_string = input(print("how many wins to finish the game? "))
+    try:
+        rounds = int(round_string)
+        flag = False
+    except (TypeError, ValueError):
+        print("This is not an integer number. Please enter a valid number")
 
-    # cpu = random.randint(1,3)
+while 1:
     player = input(print('type "r" or Rock, "s" or scissor , p or "paper"')).lower()
     if player not in entry:
         continue
     random.shuffle(cpu_deck)
-    if player == 'r':
-        if cpu_deck[0] == 's':
-            print("player wins, {} is player  and {} is cpu ".format(player, cpu_deck[0]))
-            player_score +=1
-
-    if player == 'r':
-        if cpu_deck[0] == 'p':
-            print("CPU wins, {} is player  and {} is cpu ".format(player, cpu_deck[0]))
-            cpu_score+=1
-
-    if player == 's':
-        if cpu_deck[0] == 'p':
-            print("player wins, {} is player  and {} is cpu ".format(player, cpu_deck[0]))
+    if player == 'r' or player == 'rock':
+        if cpu_deck[0] == 'scissor':
+            print("player wins, rock is player  and {} is cpu ".format(cpu_deck[0]))
             player_score += 1
-    if player == 's':
+
+    if player == 'r' or player == 'rock':
+        if cpu_deck[0] == 'paper':
+            print("CPU wins, rock is player  and {} is cpu ".format(cpu_deck[0]))
+            cpu_score += 1
+
+    if player == 's' or player == 'scissor':
+        if cpu_deck[0] == 'paper':
+            print("player wins, scissor is player  and {} is cpu ".format(cpu_deck[0]))
+            player_score += 1
+    if player == 's' or player == 'scissor':
         if cpu_deck[0] == 'r':
-            print("CPU wins, {} is player  and {} is cpu ".format(player, cpu_deck[0]))
-            cpu_score +=1
+            print("CPU wins, scissor is player  and {} is cpu ".format(cpu_deck[0]))
+            cpu_score += 1
+
+    if player == 'p' or player == 'paper':
+        if cpu_deck[0] == 'rock':
+            print("player wins, paper is player  and {} is cpu ".format(cpu_deck[0]))
+            player_score += 1
+    if player == 'p' or player == 'paper':
+        if cpu_deck[0] == 'scissor':
+            print("CPU wins, scissor is player  and {} is cpu ".format(cpu_deck[0]))
+            cpu_score += 1
 
     if player == cpu_deck[0]:
         print("Tie !, {} is player  and {} is cpu ".format(player, cpu_deck[0]))
